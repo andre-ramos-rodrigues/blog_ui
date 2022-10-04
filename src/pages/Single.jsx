0802e9a78fd5acaa47389e3a4de35aa3a3dc0382
Andre <andreramos@id.uff.br>
2022-10-04 09:49:40 -0300

    delete method

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -28,7 +28,7 @@ const Single = () => {
 
   const handleDelete = async() => {
     try{
-      await axios.delete(`https://afpoc-blog.herokuapp.com/api/posts/${id}`, {token: token}, {withCredentials: true})
+      await axios.delete(`https://afpoc-blog.herokuapp.com/api/posts/${id}`, {token: localStorage.getItem("token")}, {withCredentials: true})
       navigate("/")
     }catch(err){
       console.log(err)
